da39a6b382e1ef52ecfe441da328bbbddf108f9a
pimeo <bertrand.souriau@gmail.com>
2025-12-19 17:02:28 +0100

    feat: add french translations

---
+++ resources/lang/fr/filament-authentication-log.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'navigation.group' => 'Connexions',
+
+    'navigation.authentication-log.label' => "Journal d'authentification",
+    'navigation.authentication-log.plural-label' => "Journaux d'authentification",
+
+    'table.heading' => "Journaux d'authentification",
+    'table.empty' => "Aucun information d'authentification trouvée",
+
+    'column.authenticatable' => 'Utilisateur authentifié',
+    'column.ip_address' => 'Adresse IP',
+    'column.user_agent' => 'Agent utilisateur',
+    'column.login_at' => 'Connecté le',
+    'column.login_successful' => 'Connexion réussie',
+    'column.logout_at' => 'Déconnecté à',
+    'column.cleared_by_user' => "Effacé par l'utilisateur",
+];
